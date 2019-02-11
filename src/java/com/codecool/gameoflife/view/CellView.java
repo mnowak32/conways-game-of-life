c5c011fb7050396282b2be52d4a12f8dfe3a3b1e
Szymon Nowak <sznowak13@gmail.com>
2019-02-11 17:37:00 +0100

    CellView now has black stroke line around them

--- src/java/com/codecool/gameoflife/view/CellView.java
+++ src/java/com/codecool/gameoflife/view/CellView.java
@@ -3,6 +3,7 @@ package com.codecool.gameoflife.view;
 import com.codecool.gameoflife.model.Cell;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.StrokeType;
 
 
 public class CellView extends Rectangle {
@@ -10,6 +11,8 @@ public class CellView extends Rectangle {
     public CellView(Cell cell) {
         super(DisplayConfig.CELL_SIDE, DisplayConfig.CELL_SIDE);
         Color color = cell.isAlive() ? Color.RED : Color.WHITE;
+        this.setStroke(Color.BLACK);
+        this.setStrokeType(StrokeType.INSIDE);
         this.setFill(color);
     }
 }
