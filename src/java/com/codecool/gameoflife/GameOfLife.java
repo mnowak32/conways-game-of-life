1b8afcbc7457a0ae7a6e40398c691b4418e0ab98
Szymon Nowak <sznowak13@gmail.com>
2019-02-11 14:27:06 +0100

    Removed bad designed thread

--- src/java/com/codecool/gameoflife/GameOfLife.java
+++ src/java/com/codecool/gameoflife/GameOfLife.java
@@ -1,19 +1,19 @@
 package com.codecool.gameoflife;
 
+import com.codecool.gameoflife.controller.GameController;
+import com.codecool.gameoflife.view.DisplayConfig;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class GameOfLife extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = new GridPane();
+        GameController gameController = new GameController();
+
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(gameController.getGameView(), DisplayConfig.SCREEN_WIDTH, DisplayConfig.SCREEN_HEIGHT));
         primaryStage.show();
     }
 
---
+++ src/java/com/codecool/gameoflife/model/Config.java
@@ -0,0 +1,4 @@
+package com.codecool.gameoflife.model;
+
+public class Config {
+}
