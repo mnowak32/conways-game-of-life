1672eb08deab74b3d880be9ca47237f785494393
Szymon Nowak <sznowak13@gmail.com>
2019-02-11 15:51:35 +0100

    Randomized cell generation

--- src/java/com/codecool/gameoflife/model/GameBoard.java
+++ src/java/com/codecool/gameoflife/model/GameBoard.java
@@ -19,7 +19,7 @@ public class GameBoard {
     private void populateTiles() {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
-                board[i][j] = new Cell(i, j);
+                board[i][j] = new Cell(i, j, Math.random() > 0.5);
             }
         }
     }
