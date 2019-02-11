32d5ba87d755f672f97863fa7c30f255ce63904f
Szymon Nowak <sznowak13@gmail.com>
2019-02-11 14:27:24 +0100

    Added gameController

--- src/java/com/codecool/gameoflife/controller/GameController.java
+++ src/java/com/codecool/gameoflife/controller/GameController.java
@@ -1,5 +1,25 @@
 package com.codecool.gameoflife.controller;
 
+import com.codecool.gameoflife.model.Config;
+import com.codecool.gameoflife.model.GameBoard;
+import com.codecool.gameoflife.view.GameView;
+
 public class GameController {
+
+    private GameBoard gameBoard = new GameBoard(Config.BOARD_SIDE,Config.BOARD_SIDE);
+    private GameView gameView = new GameView();
+
+    public GameController() {
+        this.gameView.setGameBoardView(gameBoard);
+    }
+
+    public GameBoard getGameBoard() {
+        return gameBoard;
+    }
+
+    public GameView getGameView() {
+        return gameView;
+    }
+
 
 }
