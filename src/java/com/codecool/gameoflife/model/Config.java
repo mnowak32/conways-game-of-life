46bd2de014172dd814f75cff30432258f33fc358
Szymon Nowak <sznowak13@gmail.com>
2019-02-16 00:15:23 +0100

    Changed boardSize to 80

--- src/java/com/codecool/gameoflife/model/Config.java
+++ src/java/com/codecool/gameoflife/model/Config.java
@@ -2,6 +2,6 @@ package com.codecool.gameoflife.model;
 
 public class Config {
 
-    public static final int BOARD_SIDE = 30;
+    public static final int BOARD_SIDE = 80;
     public static final int CELL_NUMBER = BOARD_SIDE * BOARD_SIDE;
 }
